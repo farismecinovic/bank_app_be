7a40c7291427aa8103cc056e8316d3aa814d1f2c
Faris <faris.mecinovic.np@gmail.com>
2021-12-27 21:58:47 +0100

    Paseto Token implemented, login route for user

--- api/server.go
+++ api/server.go
@@ -1,24 +1,48 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/farismecinovic/bankapp/db/sqlc"
+	"github.com/farismecinovic/bankapp/token"
+	"github.com/farismecinovic/bankapp/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  *db.Store
-	router *gin.Engine
+	config     util.Config
+	store      *db.Store
+	router     *gin.Engine
+	tokenMaker token.Maker
 }
 
-func NewServer(store *db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store *db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenKey)
+	if err != nil {
+		return nil, fmt.Errorf("cant create a token maker")
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/user/auth", server.loginUser)
+
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
 
@@ -30,9 +54,8 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
-}
 
+}
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
