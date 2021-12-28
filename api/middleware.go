534ad74a46bfefc38ed019d19b7dbcd3225ca247
Faris <faris.mecinovic.np@gmail.com>
2021-12-28 14:29:38 +0100

    AuthMiddleware completed

--- api/middleware.go
+++ api/middleware.go
@@ -1,12 +1,52 @@
 package api
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/farismecinovic/bankapp/token"
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	authHeaderKey  = "authorization"
+	authTypeBearer = "bearer"
+	authPayloadKey = "authorization_payload"
+)
+
 func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader(authHeaderKey)
+		if len(authHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+		fields := strings.Fields(authHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authType := strings.ToLower(fields[0])
+		if authType != authTypeBearer {
+			err := fmt.Errorf("not supported auth type %s", authType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authPayloadKey, payload)
+		ctx.Next()
 
 	}
 }
